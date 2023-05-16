b99835db95619cf6f2131c8b0ea1102a297ca58d
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-05-16 15:41:32 -0300

    feat: Requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,18 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        response = requests.get(
+            url, timeout=3, headers={"user-agent": "Fake user-agent"}
+        )
+        response.raise_for_status()
+        return response.text
+    except Exception:
+        return None
 
 
 # Requisito 2
