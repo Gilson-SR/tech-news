69851bcba64af3e41ee70df5a6b00d8fd35af9df
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-05-16 17:15:25 -0300

    feat: Requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -32,7 +32,27 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("head link[rel=canonical]::attr(href)").get()
+    title = selector.css("h1::text").get().strip()
+    timestamp = selector.css(".meta-date::text").get()
+    writer = selector.css(".author > a::text").get()
+    reading_time = int(
+        selector.css(".meta-reading-time::text").get().split(" ")[0]
+    )
+    summary = "".join(
+            selector.css(".entry-content > p:nth-of-type(1) *::text").getall()
+        ).strip()
+    category = selector.css(".category-style .label::text").get()
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": reading_time,
+        "summary": summary,
+        "category": category,
+    }
 
 
 # Requisito 5
