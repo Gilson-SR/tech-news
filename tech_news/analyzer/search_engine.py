86489aba930a2ce038e24651f371970f1c332d90
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-05-16 20:15:57 -0300

    feat: Requisito 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -29,4 +29,5 @@ def search_by_date(date):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = search_news({"category": {"$regex": category, "$options": "i"}})
+    return list(news)
